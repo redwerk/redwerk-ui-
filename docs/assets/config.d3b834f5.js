3da931ffd1d8255f0f576ae3973b9c8b22982b07
oleksandr.syvash <exortwot@gmail.com>
2022-10-27 12:31:24 +0200

    gh_pages_try_2

--- docs/assets/config.d3b834f5.js
+++
@@ -1,2 +0,0 @@
-import{d as t,r as m,a as s}from"./decorateStory.12ba3f6f.js";import"./iframe.c3a08c09.js";import"./vue.esm-bundler.f46cc1d9.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
-//# sourceMappingURL=config.d3b834f5.js.map
