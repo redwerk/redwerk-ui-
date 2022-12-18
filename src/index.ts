3dc86b3cef02e349d14de75ef2e0525999ac6be1
exoort <exortwot@gmail.com>
2022-12-18 19:24:52 +0100

    fix_build_for_nuxt

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,10 @@ import * as components from './components';
 
 const plugin = {
   install(vue: any) {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const componentName in components) {
-      // eslint-disable-next-line no-prototype-builtins,import/namespace
-      if (components.hasOwnProperty(componentName)) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        // eslint-disable-next-line import/namespace
-        vue.component(componentName, components[componentName]);
-      }
-    }
+    Object.keys(components).forEach((componentName) => {
+      // eslint-disable-next-line import/namespace
+      vue.component(componentName, components[componentName as keyof typeof components]);
+    });
   },
 };
 
